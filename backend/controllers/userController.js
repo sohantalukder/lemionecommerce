a9c8042a4eb16211e0800748abb3d3692bd27e7f
sohantalukder <mdtalukder.sohan@gmail.com>
2023-02-21 22:40:12 +0600

    create api for update user information

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -65,4 +65,27 @@ const getUserProfile = asyncHandler(async (req, res) => {
     }
 });
 
-export { authUser, getUserProfile, registerUser };
+const updateUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+    if (user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+        if (req.body.password) {
+            user.password = req.body.password;
+        }
+        const updatedUser = await user.save();
+        res.status(200).json(
+            response(200, "Ok", {
+                _id: updatedUser._id,
+                name: updatedUser.name,
+                email: updatedUser.email,
+                isAdmin: updatedUser.isAdmin,
+                token: generateToken(updatedUser._id),
+            })
+        );
+    } else {
+        res.status(404).json(response(404, "User not found!", []));
+    }
+});
+
+export { authUser, getUserProfile, registerUser, updateUserProfile };
