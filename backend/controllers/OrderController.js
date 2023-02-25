4b458071fec89dd832d887f6abc4038e6eb931c6
sohantalukder <mdtalukder.sohan@gmail.com>
2023-02-25 22:12:46 +0600

    create order place api

---
+++ backend/controllers/OrderController.js
@@ -0,0 +1,33 @@
+import asyncHandler from "express-async-handler";
+import Order from "../models/orderModel.js";
+const addOrderItems = asyncHandler(async (req, res) => {
+    const {
+        orderItems,
+        shippingAddress,
+        paymentMethod,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+    } = req.body;
+    if (orderItems && orderItems?.length === 0) {
+        res.status(400).json(response(400, "No order items!", []));
+    } else {
+        const order = new Order({
+            orderItems,
+            user: req.user._id,
+            shippingAddress,
+            paymentMethod,
+            itemsPrice,
+            taxPrice,
+            shippingPrice,
+            totalPrice,
+        });
+        const createdOrder = await order.save();
+        res.status(201).json(
+            response(201, "Order Created", { records: { createdOrder } })
+        );
+    }
+});
+
+export default addOrderItems;
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 dotenv.config();
 connectDB();
 const app = express();
@@ -14,6 +15,7 @@ app.get("/", (req, res) => {
 });
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/users", orderRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
