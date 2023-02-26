413616dbb0e49e75ebd815d1da15d430eb2b5720
sohantalukder <mdtalukder.sohan@gmail.com>
2023-02-26 21:36:42 +0600

    create api for get order details

--- backend/controllers/OrderController.js
+++ backend/controllers/OrderController.js
@@ -28,5 +28,16 @@ const addOrderItems = asyncHandler(async (req, res) => {
         res.status(201).json(response(201, "Order Created", { createdOrder }));
     }
 });
+const getOrderById = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req?.params.id).populate(
+        "user",
+        "name email"
+    );
+    if (order) {
+        res.status(200).json(response(200, "Order found", order));
+    } else {
+        res.status(404).json(response(404, "Order not found", []));
+    }
+});
 
-export default addOrderItems;
+export { addOrderItems, getOrderById };
